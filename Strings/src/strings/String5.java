cefa6f97da0c8331302e21a28495965ead8b6f37
divya <divya@DESKTOP-05C4R5A>
2021-05-23 22:49:03 +0530

    first commit

---
+++ Strings/src/strings/String5.java
@@ -0,0 +1,12 @@
+package strings;
+
+public class String5 {
+	public static void main(String[] args) {
+		String str = "Suman";
+		
+		str = str.substring(1, str.length() - 1);
+		
+		System.out.println(str);
+	}
+
+}
